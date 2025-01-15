8cdfd3399701812160afc79b151b15f6c1fc83f8
MindExpanse <yuanliaa493@gmail.com>
2025-01-15 16:07:55 +0800

    Update mainLIDC.py

--- mainLIDC.py
+++ mainLIDC.py
@@ -112,7 +112,7 @@ def main(args_opt):
                               test_acc=0)
 
         if args_opt.mode == 'eval' or args_opt.resume == True:
-            assert os.path.exists(os.path.join(args_opt.checkpoint_dir, 'model_best.pth.tar')), '指定模型文件未找到，请检查'
+            assert os.path.exists(os.path.join(args_opt.checkpoint_dir, 'model_best.pth.tar')), 'Not Found!'
             logger.info('find a checkpoint, loading checkpoint from {}'.format(args_opt.checkpoint_dir))
             best_checkpoint = torch.load(os.path.join(args_opt.checkpoint_dir, 'model_best.pth.tar'), map_location=torch.device('cpu'))
 
@@ -138,7 +138,7 @@ def main(args_opt):
                             args_opt.checkpoint_dir))
 
         else:
-            logger.info('请检查mode和resume')
+            logger.info('Please check mode and resume')
 
 
         if args_opt.mode == 'train':
